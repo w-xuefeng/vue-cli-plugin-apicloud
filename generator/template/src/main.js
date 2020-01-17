25e48cf8efb3acce8208a20cc99c755fef7b55e4
w-xuefeng <xuefeng@wangxuefeng.com.cn>
2020-01-17 12:53:34 +0800

    fix: fixed comment

--- generator/template/src/main.js
+++ generator/template/src/main.js
@@ -16,11 +16,14 @@ Vue
     el: '#app',
     render: h => h(App)
   })
-  // 链式调用 init 方法， 即创建 Vue 实例
-  // Chained-calls init method, that will create a Vue instance
+/**
+ * 链式调用 init 方法， 即创建 Vue 实例
+ * Chained-calls init method, that will create a Vue instance
+ * 
+ * 开启 PC 调试时与 new Vue() 等同
+ * Same as new vue() when turning on PC debugging
+ * 
+ * 未开启时将在 apiready 的回调中创建 Vue 实例
+ * Vue instance will be created in apiready's callback when it is not turned on
+ */
 
-  // 开启 PC 调试时与 new Vue() 等同
-  // Same as new vue() when turning on PC debugging
-
-  // 未开启时将在 apiready 的回调中创建 Vue 实例  
-  // Vue instance will be created in apiready's callback when it is not turned on
