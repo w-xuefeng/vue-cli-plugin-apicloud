02f1cf33ba5d765df463d3bdd9ad78b9eca57b4b
w-xuefeng <xuefeng@wangxuefeng.com.cn>
2020-01-19 09:43:43 +0800

    fix: fixed entry

--- entry/index.js
+++ entry/index.js
@@ -1,15 +1,13 @@
 module.exports = (api, projectOptions) => {
   const { buildConfig, tempEntry, userPagesConfigPath } = require('../config')
   const { createEntries, entriesLoader } = require('../utils')
-  require('@vue/cli-service/lib/config/app.js')(api, {
+  const options = {
     ...projectOptions,
     ...buildConfig,
-    pages: createEntries(
-      api,
-      require(api.resolve(userPagesConfigPath)),
-      tempEntry
-    )
-  })
+    pages: createEntries(api, require(api.resolve(userPagesConfigPath)), tempEntry)
+  }
+  require('@vue/cli-service/lib/config/app.js')(api, options)
+  require('@vue/cli-service/lib/config/prod.js')(api, options)
   api.chainWebpack(webpackConfig => {
     entriesLoader(api, webpackConfig, tempEntry)
   })
