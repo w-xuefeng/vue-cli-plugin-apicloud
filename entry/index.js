ffc77bd736398426ad157d8037b6b31b0bac146e
w-xuefeng <xuefeng@kcalb.wang>
2020-02-01 20:50:42 +0800

    fix: fixed [WDS] Disconnected!

--- entry/index.js
+++ entry/index.js
@@ -4,7 +4,15 @@ module.exports = (api, projectOptions) => {
   const options = {
     ...projectOptions,
     ...buildConfig,
-    pages: createEntries(api, require(api.resolve(userPagesConfigPath)), tempEntry)
+    devServer: {
+      ...projectOptions.devServer,
+      disableHostCheck: true
+    },
+    pages: createEntries(
+      api,
+      require(api.resolve(userPagesConfigPath)),
+      tempEntry
+    )
   }
   require('@vue/cli-service/lib/config/app.js')(api, options)
   require('@vue/cli-service/lib/config/prod.js')(api, options)
