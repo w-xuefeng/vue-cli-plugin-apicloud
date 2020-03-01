7eb664ff4eedef5a0034fabe1fa93f43fab25b26
w-xuefeng <xuefeng@kcalb.wang>
2020-03-01 21:02:24 +0800

    fix: modify vaq-version

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const defaultPagesConfig = require('./generator/template/src/config/pages');
 const userPagesConfigPath = 'src/config/pages';
 
-const vaqver = '^1.0.15';
+const vaqver = '^1.0.16';
 
 const tempEntry = {
   template: 'public/index.html',
